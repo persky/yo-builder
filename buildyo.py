4d32a04ef78b5b394983110bb0c217e957d956ec
Ilya Persky <ilya.persky@gmail.com>
2015-01-14 20:55:28 +0200

    Initial commit

---
+++ buildyo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import subprocess
+
+
+def parse_args():
+    # TODO: add usage
+    if len(sys.argv) != 3:
+        raise Exception("Bad arguments")
+    return sys.argv[1], sys.argv[2]
+
+
+def compile_jsx(file):
+    return subprocess.check_output(["jsx", file])
+
+
+def walk(dir):
+    jsx_bundle = ""
+    for d, dirs, files in os.walk(dir):
+        for f in files:
+            name, ext = os.path.splitext(f)
+            if ext.lower() == ".jsx":
+                jsx_bundle += compile_jsx(d + "/" + f)
+                jsx_bundle += "\n;\n"
+    return jsx_bundle
+
+
+def write_bundles(jsx_bundle, bundle_file):
+    with open(bundle_file, "w") as f:
+        f.write(jsx_bundle)
+
+
+if __name__ == "__main__":
+    project_dir, bundle_file = parse_args()
+    jsx_bundle = walk(project_dir)
+    write_bundles(jsx_bundle, bundle_file)
