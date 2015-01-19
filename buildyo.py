e05066748f48135aa9689676ca63229571241c91
Ilya Persky <ilya.persky@gmail.com>
2015-01-19 16:14:19 +0200

    Added civil argument parsing + .js bundling

--- buildyo.py
+++ buildyo.py
@@ -3,21 +3,29 @@
 import os
 import sys
 import subprocess
+import argparse
 
 
 def parse_args():
-    # TODO: add usage, add normal parsing
-    if len(sys.argv) != 3:
-        raise Exception("Bad arguments")
-    return sys.argv[1], sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source_dir", help="The dir to walk and look for files to bundle")
+    parser.add_argument("-ojs", help="Resulting js/jsx bundle filename")
+    parser.add_argument("-ocss", help="Resulting css bundle filename")
+    args = parser.parse_args()
+
+    js_bundle = args.ojs or args.source_dir + "/bundle.js"
+    css_bundle = args.ocss
+
+    return args.source_dir, js_bundle, css_bundle
 
 
 def compile_jsx(file):
     return subprocess.check_output(["jsx", file])
 
-def walk(dir):
+
+def walk(dir, parse_css):
     # TODO: check if dir exists
-    jsx_bundle = ""
+    js_bundle = ""
     css_bundle = ""
     for d, dirs, files in os.walk(dir):
         for f in files:
@@ -25,23 +33,29 @@ def walk(dir):
             fullpath = d + "/" + f
             if ext.lower() == ".jsx":
                 print "jsx>", fullpath
-                jsx_bundle += compile_jsx(fullpath)
-                jsx_bundle += "\n;\n"
-            elif ext.lower() == ".css":
+                js_bundle += compile_jsx(fullpath)
+                js_bundle += "\n;\n"
+            elif ext.lower() == ".js":
+                print "js>", fullpath
+                js_bundle += open(fullpath, "r").read()
+                js_bundle += "\n;\n"
+            elif parse_css and ext.lower() == ".css":
                 print "css>", fullpath
                 css_bundle += open(fullpath, "r").read()
                 css_bundle += "\n;\n"
-    return jsx_bundle, css_bundle
+    return js_bundle, css_bundle
 
 
 def write_file(bundle, filename):
-    # TODO: check that bundle is not empty
     with open(filename, "w") as f:
         f.write(bundle)
 
 
 if __name__ == "__main__":
-    project_dir, bundle_target_dir = parse_args()
-    jsx_bundle, css_bundle = walk(project_dir)
-    write_file(jsx_bundle, bundle_target_dir + "/bundle.js")
-    write_file(css_bundle, bundle_target_dir + "/bundle.css")
+    source_dir, js_bundle_name, css_bundle_name = parse_args()
+    parse_css = bool(css_bundle_name)
+    js_bundle, css_bundle = walk(source_dir, parse_css)
+    write_file(js_bundle, js_bundle_name)
+    if parse_css:
+        write_file(css_bundle, css_bundle_name)
+
